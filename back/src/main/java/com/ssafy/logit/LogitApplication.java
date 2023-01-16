42ba8a824e782e4239cd4f59440a45fbedb2063c
공진호 <wlsgh7608@naver.com>
2023-01-16 13:35:53 +0900

    [FEAT] #S08P12A609-10 백 프로젝트 생성

---
+++ back/src/main/java/com/ssafy/logit/LogitApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.logit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogitApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/ssafy/logit/LogitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.logit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
