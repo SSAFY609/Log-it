f71706597c3826e860d61f94219721d41fa2bedd
공진호 <wlsgh7608@naver.com>
2023-01-16 17:04:28 +0900

    [FEAT] #S08P12A609-44 db 연동 확인

--- back/src/main/java/com/ssafy/logit/model/user/entity/User.java
+++ back/src/main/java/com/ssafy/logit/model/user/entity/User.java
@@ -1,4 +1,15 @@
 package com.ssafy.logit.model.user.entity;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
 }
