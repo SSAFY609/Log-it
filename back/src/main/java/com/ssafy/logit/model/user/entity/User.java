6bddd8c11756c5a20aef3f038a3a11c0ca79ea8d
김설희 <2750seolhee@naver.com>
2023-01-16 17:23:42 +0900

    [REFACT] #S08P12A609-18 리팩토링

--- back/src/main/java/com/ssafy/logit/model/user/entity/User.java
+++ back/src/main/java/com/ssafy/logit/model/user/entity/User.java
@@ -1,15 +1,14 @@
 package com.ssafy.logit.model.user.entity;
 
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class User {
+
     @Id
     @GeneratedValue
     private Long id;
     private String name;
-
 }
