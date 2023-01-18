5a7c2a6b14aec18ec3698da364cb253523a99378
김설희 <2750seolhee@naver.com>
2023-01-18 11:09:39 +0900

    [UPDATE] #S08P12A609-12 config 파일 생성 및 User Repository 연결

--- back/src/main/java/com/ssafy/logit/config/MvcConfig.java
+++ back/src/main/java/com/ssafy/logit/config/MvcConfig.java
@@ -1,11 +1,8 @@
 package com.ssafy.logit.config;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig  implements WebMvcConfigurer {
-
-
 }
---
+++ back/src/main/java/com/ssafy/logit/config/RootContextConfig.java
@@ -0,0 +1,10 @@
+package com.ssafy.logit.config;
+
+import com.ssafy.logit.model.user.repository.UserRepo;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackageClasses = {UserRepo.class})
+public class RootContextConfig {
+}
