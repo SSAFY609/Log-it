fd191d2ef1ba64c0c636ed6c527db3f1aa3b341d
김나현 <skgusdl7403@naver.com>
2023-01-19 12:45:51 +0900

    [UPDATE] #S08P12A609-67 router history 모드로 변경

--- front/logit/src/router/index.js
+++ front/logit/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SearchView from '../views/SearchView.vue'
 import EventView from '../views/EventView.vue'
@@ -28,168 +28,165 @@ import UserFindPassword from '../components/user/UserFindPassword.vue'
 import PageSetting from '../components/etc/PageSetting.vue'
 import DeveloperList from '../components/etc/DeveloperList.vue'
 
-const routes = [
-  
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView,
-    children: [
-      {
-        path: '',
-        name: 'MainPage',
-        component: MainPage
-      },
-      {
-        path: 'timeline',
-        name: 'TimeLine',
-        component: TimeLine
-      }
-    ]
-  },
-  {
-    path: '/search',
-    name: 'SearchView',
-    component: SearchView,
-    children: [
-      {
-        path: '',
-        name: 'SearchResult',
-        component: SearchResult
-      }
-    ]
-  },
-  {
-    path: '/event',
-    name: 'EventView',
-    component: EventView,
-    children: [
-      {
-        path: '',
-        name: 'EventList',
-        component: EventList
-      },
-      {
-        path: ':eventId',
-        name: 'EventProgress',
-        component: EventProgress
-      },
-      {
-        path: ':eventId/create',
-        name: 'ProgressCreate',
-        component: ProgressCreate
-      },
-      {
-        path: ':eventId/:progressId',
-        name: 'ProgressDetail',
-        component: ProgressDetail
-      }
-    ]
-  },
-  {
-    path: '/job',
-    name: 'JobView',
-    component: JobView,
-    children: [
-      {
-        path: '',
-        name: 'JobList',
-        component: JobList
-      },
-      {
-        path: ':jobId',
-        name: 'JobProgress',
-        component: JobProgress
-      }
-    ]
-  },
-  {
-    path: '/profile',
-    name: 'ProfileView',
-    component: ProfileView,
-    children: [
-      {
-        path: '',
-        name: 'ProfilePage',
-        component: ProfilePage
-      },
-      {
-        path: 'update',
-        name: 'UpdateProfile',
-        component: UpdateProfile
-      },
-      {
-        path: 'checkpw',
-        name: 'CheckPassword',
-        component: CheckPassword
-      },
-      {
-        path: 'password',
-        name: 'UpdatePassword',
-        component: UpdatePassword
-      }
-    ]
-  },
-  {
-    path: '/user',
-    name: 'UserView',
-    component: UserView,
-    children: [
-      {
-        path: 'login',
-        name: 'UserLogin',
-        component: UserLogin
-      },
-      {
-        path: 'email',
-        name: 'UserEmail',
-        component: UserEmail
-      },
-      {
-        path: 'password',
-        name: 'UserPassword',
-        component: UserPassword
-      },
-      {
-        path: 'photo',
-        name: 'UserPhoto',
-        component: UserPhoto
-      },
-      {
-        path: 'profile',
-        name: 'UserProfile',
-        component: UserProfile
-      },
-      {
-        path: 'findpw',
-        name: 'UserFindPassword',
-        component: UserFindPassword
-      }
-    ]
-  },
-  {
-    path: '/etc',
-    name: 'EtcView',
-    component: EtcView,
-    children: [
-      {
-        path: '/setting',
-        name: 'PageSetting',
-        component: PageSetting
-      },
-      {
-        path: '/developers',
-        name: 'DeveloperList',
-        component: DeveloperList
-      }
-    ]
-  }
-
-]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+      children: [
+        {
+          path: '',
+          name: 'MainPage',
+          component: MainPage
+        },
+        {
+          path: 'timeline',
+          name: 'TimeLine',
+          component: TimeLine
+        }
+      ]
+    },
+    {
+      path: '/search',
+      name: 'SearchView',
+      component: SearchView,
+      children: [
+        {
+          path: '',
+          name: 'SearchResult',
+          component: SearchResult
+        }
+      ]
+    },
+    {
+      path: '/event',
+      name: 'EventView',
+      component: EventView,
+      children: [
+        {
+          path: '',
+          name: 'EventList',
+          component: EventList
+        },
+        {
+          path: ':eventId',
+          name: 'EventProgress',
+          component: EventProgress
+        },
+        {
+          path: ':eventId/create',
+          name: 'ProgressCreate',
+          component: ProgressCreate
+        },
+        {
+          path: ':eventId/:progressId',
+          name: 'ProgressDetail',
+          component: ProgressDetail
+        }
+      ]
+    },
+    {
+      path: '/job',
+      name: 'JobView',
+      component: JobView,
+      children: [
+        {
+          path: '',
+          name: 'JobList',
+          component: JobList
+        },
+        {
+          path: ':jobId',
+          name: 'JobProgress',
+          component: JobProgress
+        }
+      ]
+    },
+    {
+      path: '/profile',
+      name: 'ProfileView',
+      component: ProfileView,
+      children: [
+        {
+          path: '',
+          name: 'ProfilePage',
+          component: ProfilePage
+        },
+        {
+          path: 'update',
+          name: 'UpdateProfile',
+          component: UpdateProfile
+        },
+        {
+          path: 'checkpw',
+          name: 'CheckPassword',
+          component: CheckPassword
+        },
+        {
+          path: 'password',
+          name: 'UpdatePassword',
+          component: UpdatePassword
+        }
+      ]
+    },
+    {
+      path: '/user',
+      name: 'UserView',
+      component: UserView,
+      children: [
+        {
+          path: 'login',
+          name: 'UserLogin',
+          component: UserLogin
+        },
+        {
+          path: 'email',
+          name: 'UserEmail',
+          component: UserEmail
+        },
+        {
+          path: 'password',
+          name: 'UserPassword',
+          component: UserPassword
+        },
+        {
+          path: 'photo',
+          name: 'UserPhoto',
+          component: UserPhoto
+        },
+        {
+          path: 'profile',
+          name: 'UserProfile',
+          component: UserProfile
+        },
+        {
+          path: 'findpw',
+          name: 'UserFindPassword',
+          component: UserFindPassword
+        }
+      ]
+    },
+    {
+      path: '/etc',
+      name: 'EtcView',
+      component: EtcView,
+      children: [
+        {
+          path: '/setting',
+          name: 'PageSetting',
+          component: PageSetting
+        },
+        {
+          path: '/developers',
+          name: 'DeveloperList',
+          component: DeveloperList
+        }
+      ]
+    }
+  ]
 })
 
 
