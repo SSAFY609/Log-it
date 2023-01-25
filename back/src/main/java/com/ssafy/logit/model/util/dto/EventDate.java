02055815d72e437fa2295dbacc7d18db76a8b70b
공진호 <wlsgh7608@naver.com>
2023-01-25 13:14:18 +0900

    [FEAT] #S08P12A609-22 EventDate 엔티티 수정

--- back/src/main/java/com/ssafy/logit/model/util/dto/EventDate.java
+++ back/src/main/java/com/ssafy/logit/model/util/dto/EventDate.java
@@ -1,14 +1,43 @@
 package com.ssafy.logit.model.util.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.bytebuddy.asm.Advice;
+
 import javax.persistence.Embeddable;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Embeddable
+@Getter
+@Setter(AccessLevel.PRIVATE)
+@Schema
 public class EventDate {
 
+    @Schema(description = "생성날짜")
+    private LocalDate startDate;
+    @Schema(description = "종료날짜")
+    private LocalDate endDate;
 
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
+
+
+    //생성자
+    public static EventDate createEventDate(LocalDate startDate,LocalDate endDate){
+        EventDate eventDate = new EventDate();
+        if(isValidate(startDate,endDate)){
+            eventDate.setStartDate(startDate);
+            eventDate.setEndDate(endDate);
+            return eventDate;
+        }
+        return null;
+    }
+
+    private static boolean isValidate(LocalDate startDate, LocalDate endDate){
+        return endDate.isAfter(startDate);
+    }
+
 
 }
