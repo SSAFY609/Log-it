6fdc3ed43c15d9323829d7d5f0c0f1203d6aff23
wlsgh7608 <wlsgh7608@naver.com>
2023-01-29 03:23:09 +0900

    [FIX] #S08P12A609-134 파일 경로 수정

--- back/src/main/java/com/ssafy/logit/config/MvcConfig.java
+++ back/src/main/java/com/ssafy/logit/config/MvcConfig.java
@@ -1,8 +1,18 @@
 package com.ssafy.logit.config;
 
+import com.ssafy.logit.interceptor.JwtInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig  implements WebMvcConfigurer {
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new JwtInterceptor())
+                .addPathPatterns("/**") // 기본 적용 경로
+                .excludePathPatterns("/user/login/**","/swagger-ui/**","/api-docs/**"); // 적용 제외 경로
+    }
 }
