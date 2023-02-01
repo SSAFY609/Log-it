748f16b804d684112610c02ba9eeb38d8eb91e18
wlsgh7608 <wlsgh7608@naver.com>
2023-02-02 08:43:31 +0900

    [FEAT] #S08P12A609-164 ResultDto 수정

--- back/src/main/java/com/ssafy/logit/model/common/ResultDto.java
+++ back/src/main/java/com/ssafy/logit/model/common/ResultDto.java
@@ -1,10 +1,11 @@
 package com.ssafy.logit.model.common;
 
 import lombok.AllArgsConstructor;
-
+import lombok.Data;
 
 
 @AllArgsConstructor
+@Data
 public class ResultDto<T>{
     // 개수와 data을 반환해주는 dto클래스
     private int count;
