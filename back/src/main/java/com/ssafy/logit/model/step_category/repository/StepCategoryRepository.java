883ab575d18269a6da21ce8aee13584a6d5f2365
공진호 <wlsgh7608@naver.com>
2023-02-02 16:23:39 +0900

    [FEAT] #S08P12A609-184 채용전형 Repository 생성

---
+++ back/src/main/java/com/ssafy/logit/model/step_category/repository/StepCategoryRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.logit.model.step_category.repository;
+
+import com.ssafy.logit.model.step_category.entity.StepCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StepCategoryRepository extends JpaRepository<StepCategory,Long> {
+
+}
