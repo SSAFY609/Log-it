0e61d4ed13f9b398a31c6f34359b04ef5d3a2aa5
wlsgh7608 <wlsgh7608@naver.com>
2023-02-04 23:21:49 +0900

    [FEAT] #S08P12A609-175 채용 전형 dto 직렬화추가

--- back/src/main/java/com/ssafy/logit/model/step_category/dto/CreateStepCategoryRequest.java
+++ back/src/main/java/com/ssafy/logit/model/step_category/dto/CreateStepCategoryRequest.java
@@ -2,18 +2,19 @@ package com.ssafy.logit.model.step_category.dto;
 
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.ssafy.logit.model.common.EventDate;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.ssafy.logit.model.common.ResultStatus;
+import com.ssafy.logit.model.step_category.dto.serializer.JobCategoryDeserializer;
+import com.ssafy.logit.model.step_category.dto.serializer.JobCateogorySerializer;
 import com.ssafy.logit.model.step_category.entity.JobCategory;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 @Data
-@AllArgsConstructor
 public class CreateStepCategoryRequest {
     private Long jobEventId;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
@@ -22,4 +23,14 @@ public class CreateStepCategoryRequest {
     private ResultStatus resultStatus;
     private JobCategory jobCategory;
 
+
+    @JsonSerialize(using = JobCateogorySerializer.class)
+    public JobCategory getJobCategory() {
+        return jobCategory;
+    }
+
+    @JsonDeserialize(using = JobCategoryDeserializer.class)
+    public void setJobCategory(JobCategory jobCategory) {
+        this.jobCategory = jobCategory;
+    }
 }
