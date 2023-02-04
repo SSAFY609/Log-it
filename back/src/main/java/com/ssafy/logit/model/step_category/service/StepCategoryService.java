5ac588fbdbda7ddbe7d162b20436b935e3c50885
공진호 <wlsgh7608@naver.com>
2023-02-05 01:50:24 +0900

    [FIX] #S08P12A609-172 채용전형 불필요한 출력문 제거

--- back/src/main/java/com/ssafy/logit/model/step_category/service/StepCategoryService.java
+++ back/src/main/java/com/ssafy/logit/model/step_category/service/StepCategoryService.java
@@ -14,7 +14,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.NoSuchElementException;
 
 
@@ -29,16 +28,7 @@ public class StepCategoryService {
 
     @Transactional
     public StepCategory create(User user, CreateStepCategoryRequest request) {
-
-        List<JobEvent> all = jobRepository.findAll();
-
-        System.out.println("all.size() = " + all.size());
-        for (JobEvent jobEvent : all) {
-            System.out.println("jobEvent = " + jobEvent.getId());
-        }
-        System.out.println("request.getJobEventId() = " + request.getJobEventId());
         JobEvent jobEvent = jobRepository.findById(request.getJobEventId()).orElseThrow(NoSuchElementException::new);
-        log.info("직업 이벤트 {} ", jobEvent);
         User eventUser = jobEvent.getUser();
         checkUser(user, eventUser);
 
@@ -55,11 +45,9 @@ public class StepCategoryService {
 
     @Transactional
     public StepCategory update(User user, Long id, UpdateStepCategoryRequest request) {
-
         StepCategory stepCategory = stepCategoryRepository.findById(id).orElseThrow(NoSuchElementException::new);
         checkUser(user, stepCategory);
         StepCategory updateCategory = stepCategory.updateCategory(request.getTypeDate(), request.getResultStatus());
-        System.out.println("updateCategory.getTypeDate() = " + updateCategory.getTypeDate());
         return updateCategory;
     }
 
@@ -71,15 +59,13 @@ public class StepCategoryService {
     }
 
     private void checkUser(User user, StepCategory stepCategory) {
-        if (user != stepCategory.getJobEvent().getUser()) {
+        // 유저 체크
+        if (user != stepCategory.getJobEvent().getUser()) { //해당 유저가 아닌경우
             throw new DifferentUserException();
         }
     }
 
     private void checkUser(User user, User eventUser) {
-        System.out.println("user = " + user);
-        System.out.println("eventUser = " + eventUser);
-        System.out.println("user==eventUser = " + (user == eventUser));
         if (user != eventUser) { //해당 유저가 아닌 경우
             throw new DifferentUserException();
         }
