9630460870c817aec43865817392eacc5b5a3136
wlsgh7608 <wlsgh7608@naver.com>
2023-02-04 23:22:56 +0900

    [FEAT] #S08P12A609-172 채용 전형 Controller 추가

---
+++ back/src/main/java/com/ssafy/logit/controller/step_category/StepCategoryController.java
@@ -0,0 +1,78 @@
+package com.ssafy.logit.controller.step_category;
+
+
+import com.ssafy.logit.model.job.service.JobService;
+import com.ssafy.logit.model.step_category.dto.CreateStepCategoryRequest;
+import com.ssafy.logit.model.step_category.dto.CreateStepCategoryResponse;
+import com.ssafy.logit.model.step_category.entity.StepCategory;
+import com.ssafy.logit.model.step_category.service.StepCategoryService;
+import com.ssafy.logit.model.user.entity.User;
+import com.ssafy.logit.model.user.repository.UserRepository;
+import com.ssafy.logit.model.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/step-category")
+@Tag(name = "step-category", description = "채용전형분류 API")
+@Slf4j
+public class StepCategoryController {
+    private final StepCategoryService stepCategoryService;
+    private final JobService jobService;
+    private final UserService userService;
+    private final UserRepository userRepository;
+
+
+    @Operation(summary = "채용 전형 생성 ", description = "채용 전형 데이터를 생성합니다. RequestBody에 취업이벤트의 id가 필요합니다.")
+    @PostMapping
+    public ResponseEntity<CreateStepCategoryResponse> create(@RequestAttribute String email, @RequestBody @Validated  CreateStepCategoryRequest request) {
+        User user = getUser(email);
+        StepCategory stepCategory = stepCategoryService.create(user, request);
+        CreateStepCategoryResponse response = new CreateStepCategoryResponse(stepCategory);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+
+    @Operation(summary = "단일 채용 전형 조회 ", description = "id값을 이용하여 단일 채용 전형 데이터를 조회합니다")
+    @GetMapping("/{id}")
+    public ResponseEntity<CreateStepCategoryResponse> get(@PathVariable Long id) {
+        StepCategory stepCategory = stepCategoryService.get(id);
+        CreateStepCategoryResponse response = new CreateStepCategoryResponse(stepCategory);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @Operation(summary = "채용 전형 수정 ", description = "id값을 이용하여 단일 채용 전형 데이터를 수정합니다. 취업여정 이벤트의 유저와 동일해야 합니다.")
+    @PutMapping("/{id}")
+    public ResponseEntity<CreateStepCategoryResponse> update(
+            @RequestAttribute String email
+            , @PathVariable Long id, @RequestBody @Validated CreateStepCategoryRequest request) {
+        log.info("채용 전형 수정 Controller {} ",request);
+        User user = getUser(email);
+        StepCategory updateCategory = stepCategoryService.update(user, id, request);
+        log.info("수정 : {} ", updateCategory);
+        CreateStepCategoryResponse response = new CreateStepCategoryResponse(updateCategory);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+
+    @Operation(summary = "채용 전형 삭제 ", description = "id값을 이용하여 단일 채용 전형 데이터를 삭제합니다. 취업여정 이벤트의 유저와 동일해야 합니다.")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(
+            @RequestAttribute String email, @PathVariable Long id) {
+        User user = getUser(email);
+        stepCategoryService.delete(user, id);
+
+        return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
+    }
+
+    private User getUser(String email) {
+        return userService.getUserEntity(email);
+    }
+}
