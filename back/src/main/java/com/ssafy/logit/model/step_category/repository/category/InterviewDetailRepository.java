2e9b1bf1d2e165f8878e1b3179f6eaabcff2fd50
공진호 <wlsgh7608@naver.com>
2023-02-05 04:20:26 +0900

    [FEAT] #S08P12A609-252 면접상세 비즈니스 구현

---
+++ back/src/main/java/com/ssafy/logit/model/step_category/repository/category/InterviewDetailRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.logit.model.step_category.repository.category;
+
+import com.ssafy.logit.model.step_category.entity.category.InterviewDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterviewDetailRepository extends JpaRepository<InterviewDetail,Long> {
+
+}
