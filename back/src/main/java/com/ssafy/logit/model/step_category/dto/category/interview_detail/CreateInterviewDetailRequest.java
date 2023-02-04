04ef60d144c43fabde58118ccedd2c454e8bca69
공진호 <wlsgh7608@naver.com>
2023-02-05 03:42:41 +0900

    [FIX] #S08P12A609-251 면접상세 dto 필드 수정

---
+++ back/src/main/java/com/ssafy/logit/model/step_category/dto/category/interview_detail/CreateInterviewDetailRequest.java
@@ -0,0 +1,38 @@
+package com.ssafy.logit.model.step_category.dto.category.interview_detail;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.ssafy.logit.model.step_category.dto.serializer.InterviewCategoryDeserializer;
+import com.ssafy.logit.model.step_category.dto.serializer.InterviewCategorySerializer;
+import com.ssafy.logit.model.step_category.entity.category.InterviewCategory;
+import lombok.Data;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class CreateInterviewDetailRequest {
+
+
+    @NotNull
+    private Long stepId;
+    @Max(200)
+    private String question;
+    @Max(1000)
+    private String answer;
+
+    private InterviewCategory interviewCategory;
+
+
+    @JsonSerialize(using = InterviewCategorySerializer.class)
+    public InterviewCategory getInterviewCategory() {
+        return interviewCategory;
+    }
+
+    @JsonDeserialize(using = InterviewCategoryDeserializer.class)
+    public void setInterviewCategory(InterviewCategory interviewCategory) {
+        this.interviewCategory = interviewCategory;
+    }
+
+
+}
