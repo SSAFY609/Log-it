bcd2c23789ad1d996d199ff81f7045ee1ce39763
HOONY-LEE <leesh940312@gmail.com>
2023-02-05 21:18:14 +0900

    [FIX] fix bug

--- front/logit/src/components/job/JobProgress.vue
+++ front/logit/src/components/job/JobProgress.vue
@@ -8,108 +8,39 @@
       </div>
       <div class="tab_area lay3">
         <div v-for="i in datas.category" :key="i">
-          <div class="tab_item_box f_main">{{ i }}</div>
+          <div class="tab_item_box f_main selected_item hover_cursor">{{ i }}</div>
         </div>
-        <div class="add_btn f_main">+전형 추가</div>
+        <div class="add_btn f_main hover_cursor">+전형 추가</div>
+      </div>
+      <div class="contents_area">
+        <div v-if="clicked=='서류'" class="contents_box lay2">
+          {{datas.details[0].answer}}
+          <div class="q_input_area">
+            <div class="q_input_box">
+              <input class="q_text" type="text" placeholder="질문을 추가하세요." autofocus>
+            </div>
+            <div class="a_input_box">
+              <textarea  class="a_text" placeholder="답변을 작성하세요."></textarea>
+            </div>
+            
+          </div>
+          <div class="q_btn">+문답추가</div>
+          서류전형
+        </div>
+        <div v-if="clicked=='코테'" class="contents_box lay2">
+          코딩테스트
+        </div>
+        <div v-if="clicked=='면접'" class="contents_box lay2">
+          면접전형
+        </div>
+        <div v-if="clicked=='추가'" class="contents_box lay2">
+          전형추가
+        </div>
       </div>
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      <!-- <v-card color="basil" class="top_area">
-        <v-card-title class="text-center justify-center py-4 title_area">
-          <h1 class="font-weight-bold text-h2 text-basil">
-          {{ datas.name}} 취업여정
-          </h1>
-          <div> 2022.01.02 ~ 진행중</div>
-        </v-card-title>
-
-    <v-tabs
-      v-model="tab"
-      bg-color="transparent"
-      color="basil"
-      grow
-    >
-      <v-tab
-        class="tab_text_area"
-        v-for="item in datas.category"
-        :key="item"
-        :value="item"
-      >
-      {{ item }}
-      </v-tab>
-    </v-tabs>
-    <v-window v-model="tab">
-      <v-window-item
-        v-for="item in datas.category"
-        :key="item"
-        :value="item" 
-      >
-    <v-card
-      class="job_text_area"
-      color="#fff099"
-      flat
-    >
-        <v-card-text class="job_text lay2">
-          <div v-if="update">
-            <div> 게시글 수정 모드</div>
-            <v-btn @click="update=false"> 저장 </v-btn>
-            <br>
-            <div>{{ datas.details[0].question }}</div>
-            <v-textarea
-              class="lay3 input_item_box"
-              bg-color="transparent"
-              variant=null
-              v-model:model-value="datas.details[0].answer"
-            >{{ datas.details }}</v-textarea>
-            {{ datas.details }}
-            
-          </div>
-          <div v-else>
-            <div> 게시글 읽기 모드</div>
-            <v-btn @click="update=true"> 수정 </v-btn>
-            <br>
-            <div>{{ datas.details[0].question }}</div>
-            <v-textarea
-              class="lay3 input_item_box"
-              bg-color="transparent"
-              variant=null
-              readonly
-              v-model:model-value="datas.details[0].answer"
-            >{{ datas.details }}</v-textarea>
-            {{ datas.details }}
-          </div>  
-        </v-card-text>
-        </v-card>
-      </v-window-item>
-    </v-window>
-  </v-card> -->
     </div>
    </div>
  </template>
@@ -131,9 +62,11 @@
         items: [
           '서류전형', '코딩테스트', '기술면접',
         ],
+        data: "데이터",
         text: '서류전형은 어쩌구 저쩌구 더미 데이터이다!!!',
         create_content: '',
         update: false,
+        clicked: '서류',
         datas: {
           name: "카카오",
           startDate: "2021-01-03",
@@ -151,49 +84,6 @@
  
  <style scoped>
 
- /* .input_item_box {
-  height: 100px;
- }
- .title_area {
-  padding-top: 34px!important;
-  padding-bottom: 2px!important;
- }
- .top_area {
-  border-radius: 10px;
- }
-
- .tab_text_area {
-  font-size: 24px;  
- }
-
- .job_text {
-    font-size: 24px;
-    height: 560px;
-    font-family: event;
- } */
-
- /* .job_text_area {
-  height: 600px;
-  padding: 30px;
- } */
-/* Helper classes */
-/* .bg-basil {
-  background-color: #fff099 !important;
-}
-
-.text-basil {
-  color: #191919 !important;
-}
-
-
-.editor_area {
-  font-size: 30px;
-}
-
-.detail-form {
-  height: 400px;
-}
-*/
 .container {
   display: flex;
   flex-direction: column;
@@ -227,19 +117,17 @@ span {
 }
 
 .tab_area {
-  height: 60px;
+  height: 40px;
   display: flex;
   align-items: center;
-
-
+  margin-top: 20px;
 }
 
 .tab_item_box {
-  height: 60px;
+  height: 40px;
   background-color: rgb(235, 235, 235);
-  border-top-left-radius: 8px;
-  border-top-right-radius: 8px;
-  margin-right: 10px;
+  border-radius: 6px;
+  margin-right: 6px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -247,17 +135,91 @@ span {
 }
 
 .add_btn {
-  height: 60px;
+  height: 40px;
   background-color: rgb(235, 235, 235);
-  border-top-left-radius: 8px;
-  border-top-right-radius: 8px;
-  margin-right: 10px;
+  border-radius: 6px;
+  margin-right: 6px;
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 0px 30px;
-  font-family: appleM;
+  font-family: appleL;
+}
+.selected_item {
+  background-color: #FF0A54 ;
+  color: white;
+  z-index: 1000;
+}
+.contents_area {
+  margin-top: 16px;
+  width: 800px;
+  height: 590px;
+  background-color: rgb(255, 248, 254);
+  border-radius: 6px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+}
+.contents_box {
+  margin-top: 30px;
+  width: 700px;
+  height: 500px;
+}
+.q_btn {
+  width: 100px;
+  height: 40px;
+  background-color: #FF0A54;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  border-radius: 6px;
+}
+.q_input_area {
+  width: 100%;
+  height: 400px;
+  background-color: green;
+}
+.q_input_box {
+  height: 50px;
+  background-color: beige;
+  display: flex;
+
+}
+.a_input_box {
+  height: 300px;
+  margin-top: 10px;
+  border-radius: 6px;
+  background-color: rgba(255,255,255,0.8);
+  
 }
 
+input{
+  padding-left: 12px;
+  width: 700px;
+  background-color: rgba(255,255,255,0.8);
+  border-radius: 6px;
+}
+textarea {
+  padding-left: 12px;
+  width: 700px;
+  height: 100%;
+  background-color: rgba(255,255,255,0.8);
+  border-radius: 6px;
+}
+
+input:focus {
+  outline: none;
+}
+textarea:focus {
+  outline: none;
+}
+
+.a_text {
+  width: 700px;
+  height: 100%;
+  display: block;
+}
 
  </style>
