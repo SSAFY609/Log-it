8b23594416a463043c06d0b8ea747aff35e18f76
공진호 <wlsgh7608@naver.com>
2023-02-07 14:48:45 +0900

    [FIX] #S08P12A609-242 메소드 수정

--- back/src/main/java/com/ssafy/logit/model/step_category/service/category/DocumentService.java
+++ back/src/main/java/com/ssafy/logit/model/step_category/service/category/DocumentService.java
@@ -15,7 +15,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
