e85fae7f6d291a47f26509f59e79477ebe2fc9f5
공진호 <wlsgh7608@naver.com>
2023-02-08 10:49:15 +0900

    [FEAT] #S08P12A609-312 채용전형(코테) 카테고리 추가

--- back/src/main/java/com/ssafy/logit/model/step_category/entity/category/AlgoCategory.java
+++ back/src/main/java/com/ssafy/logit/model/step_category/entity/category/AlgoCategory.java
@@ -1,9 +1,21 @@
 package com.ssafy.logit.model.step_category.entity.category;
 
 public enum AlgoCategory {
-    BFS("BFS"),DFS("DFS"),GRAPH("그래프 구현"),SIMULATION("구현"),STRING("문자열"),SORT("정렬"),
-    BINARYSEARCH("이분탐색"),DATASTRUCTURE("자료 구조"),BRUTEFORCE("완전탐색"),HEAP("힙"),GRAPHSEARCH("그래프 탐색"),
-    DP("동적 프로그래밍");
+    BFS("BFS"),
+    DFS("DFS"),
+    GRAPH("그래프 구현"),
+    SIMULATION("구현"),
+    STRING("문자열"),
+    SORT("정렬"),
+    BINARYSEARCH("이분탐색"),
+    DATASTRUCTURE("자료 구조"),
+    BRUTEFORCE("완전탐색"),
+    HEAP("힙"),
+    TREE("트리"),
+    GRAPHSEARCH("그래프 탐색"),
+    DP("동적 프로그래밍"),
+    GREEDY("그리디"),
+    BACKTRACKING("백 트래킹");
 
 
     private String value;
