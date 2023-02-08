2b81fdf358a540d62dddff0721dee93d3cf16f51
공진호 <wlsgh7608@naver.com>
2023-02-08 10:53:33 +0900

    [FIX] #S08P12A609-312 불필요한 import 제거

--- back/src/main/java/com/ssafy/logit/model/search/service/SearchService.java
+++ back/src/main/java/com/ssafy/logit/model/search/service/SearchService.java
@@ -1,12 +1,10 @@
 package com.ssafy.logit.model.search.service;
 
-import com.ssafy.logit.model.user.dto.UserDto;
 import com.ssafy.logit.model.user.entity.User;
 import com.ssafy.logit.model.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -17,8 +15,7 @@ public class SearchService {
     private final UserService userService;
 
     public List<User> searchUserList(String name){
-        List<User> users = userService.getUsersByName(name);
-        return users;
+        return userService.getUsersByName(name);
     }
 
 
