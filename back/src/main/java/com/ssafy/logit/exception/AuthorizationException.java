9a9c7adc771e7fce66abc45a0417e9720dfc59e8
공진호 <wlsgh7608@naver.com>
2023-02-09 17:36:20 +0900

    [FEAT] 인터셉터 예외처리 추가

---
+++ back/src/main/java/com/ssafy/logit/exception/AuthorizationException.java
@@ -0,0 +1,23 @@
+package com.ssafy.logit.exception;
+
+public class AuthorizationException extends  RuntimeException{
+    public AuthorizationException() {
+        super("JWT 토큰이 없습니다.");
+    }
+
+    public AuthorizationException(String message) {
+        super(message);
+    }
+
+    public AuthorizationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AuthorizationException(Throwable cause) {
+        super(cause);
+    }
+
+    protected AuthorizationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
