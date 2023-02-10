e111a32c985a94a9f7bcd5f856ce94c8a5975b4d
김설희 <2750seolhee@naver.com>
2023-02-10 09:08:36 +0900

    [UPDATE] #S08P12A609-297 대표 progress 조회 오류 수정

--- back/src/main/java/com/ssafy/logit/model/growth/repository/ProgressRepository.java
+++ back/src/main/java/com/ssafy/logit/model/growth/repository/ProgressRepository.java
@@ -17,11 +17,11 @@ public interface ProgressRepository  extends JpaRepository<Progress, Long> {
     @Query(value = "select * from progress where date = ?1", nativeQuery = true)
     public Optional<List<Progress>> findByDate(String date);
 
-    @Query(value = "select * from progress where date = ?1 and user_id = ?2", nativeQuery = true)
-    public Optional<Progress> getMine(String date, long userId);
+    @Query(value = "select * from progress where growth_id = ?1 date = ?2 and user_id = ?3", nativeQuery = true)
+    public Optional<Progress> getMine(long growthId, String date, long userId);
 
     @Query(value = "select * from progress where growth_id = ?1 and date = ?2 order by like_cnt desc, progress_id asc limit 1", nativeQuery = true)
-    public Optional<Progress> getFirst(long growth_id, String date);
+    public Optional<Progress> getFirst(long growthId, String date);
 
     @Query(value = "select * from progress where growth_id = ?1 and date = ?2 and user_id != ?3 order by like_cnt desc, progress_id asc", nativeQuery = true)
     public Optional<List<Progress>> getDateProgress(long growth_id, String date, long user_id);
